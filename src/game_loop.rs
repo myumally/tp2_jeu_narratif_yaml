d2730e4218ea8eacda2ec54196de3bb16e94b588
Domi <domitillevale@gmail.com>
2026-03-03 17:19:23 +0100

    TP Rust: game loop

--- src/game_loop.rs
+++ src/game_loop.rs
@@ -16,7 +16,11 @@ fn game_loop(story: &Story, state: &mut GameState){
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Erreur");
         match parse_command(&input).expect("erreur pour parse la commande").execute(story, state).expect("erreur pour exec la commande"){
-            crate::command::CommandOutcome::Quit=> quit = true,
+            crate::command::CommandOutcome::Quit => quit = true,
+            crate::command::CommandOutcome::GameOver => {
+                println!("Skill issue you're dead");
+                quit = true;
+            }
             _ => quit = false
         }
     }
